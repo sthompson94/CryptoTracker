6bf89f67d750b7ce560704628fe48dca52ff314c
Sam Thompson <electronic62794@yahoo.com>
2020-11-17 21:46:36 -0600

    get information I need and display to the page

--- cryptotracker/src/Components/Chart.js
+++ cryptotracker/src/Components/Chart.js
@@ -1,46 +1,55 @@
-import React from 'react';
+import React from "react";
 
 class Chart extends React.Component {
-
   state = {
     crypto: "",
-    price: ""
+    price: "",
+  };
+  componentDidMount() {
+    this.getData();
   }
-
 
   render() {
-    this.getData();
-    return(
-      <h1>Hello "World"</h1>
-    )
+    return (
+      <div>
+        <h1>{this.state.crypto}:</h1>
+        <h2>{this.state.price}</h2>
+      </div>
+    );
   }
 
-
- getData = () => {
-    const rp = require('request-promise');
+  getData = () => {
+    const rp = require("request-promise");
     const requestOptions = {
-      method: 'GET',
-      uri: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
+      method: "GET",
+      uri:
+        "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest",
       qs: {
-        'start': '1',
-        'limit': '50',
-        'convert': 'USD'
+        start: "1",
+        limit: "50",
+        convert: "USD",
       },
       headers: {
-        'X-CMC_PRO_API_KEY': '2ac9ef9a-06f2-4a4e-9f5e-ef19c0c9d1d6'
+        "X-CMC_PRO_API_KEY": "2ac9ef9a-06f2-4a4e-9f5e-ef19c0c9d1d6",
       },
       json: true,
-      gzip: true
+      gzip: true,
     };
-    
-    rp(requestOptions).then(response => {
-      console.log('API call response:', response);
 
-    }).catch((err) => {
-      console.log('API call error:', err.message);
-    });
-}
-    
+    rp(requestOptions)
+      .then((response) => {
+        console.log("API call response:", response);
+
+        this.setState({
+          crypto: response.data[0].name,
+          price: response.data[0].quote.USD.price,
+        });
+      })
+      .catch((err) => {
+        console.log("API call error:", err.message);
+      });
+    console.log(this.state);
+  };
 }
 
 export default Chart;
